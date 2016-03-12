70b43e31337c699f427eb117157d4cde46be4ec1
KimberlyAbuschinow <kabuschinow@live.com>
2016-03-12 13:52:15 -0500

    Added Functions to avoid repetition

--- UOP_Calculator_MLB406/ViewController.swift
+++ UOP_Calculator_MLB406/ViewController.swift
@@ -9,16 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    // MARK:  Outlets
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var firstNumTextField: UITextField!
+    @IBOutlet weak var secondNumTextField: UITextField!
+    
+    
+    // MARK:  Variables and Properties
+    var firstNum: Double = 0
+    var secondNum: Double = 0
+    var result: Double = 0
+    
+    
 
+    // MARK:  Functions and Override Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func displayResults() {
+        resultsLabel.text = "\(result)"
     }
+    
+    func getData() {
+        
+        firstNum = Double(firstNumTextField.text!)!
+        secondNum = Double(secondNumTextField.text!)!
+    }
+    
+    func clearData() {
+        firstNum = 0
+        secondNum = 0
+        firstNumTextField.text = ""
+        secondNumTextField.text = ""
+        result = 0
+        resultsLabel.text = ""
+    }
+   
+    // MARK:  Actions
+    @IBAction func onAddPressed(sender: UIButton) {
+        
+        
+        getData()
+        let calcAdd = SimpleCalculator(firstNumber: firstNum, secondNumber: secondNum)
+        result = calcAdd.add(firstNum, secondNumber: secondNum)
+        displayResults()
+        
+        
+    }
+    
+    @IBAction func onSubtractPressed(sender: UIButton) {
+        getData()
+        let calcSub = SimpleCalculator(firstNumber: firstNum, secondNumber: secondNum)
+        result = calcSub.subtract(firstNum, secondNumber: secondNum)
+        displayResults()
+    }
+    
+    
+    @IBAction func onMultiplyPressed(sender: UIButton) {
+        getData()
+        let calcMult = SimpleCalculator(firstNumber: firstNum, secondNumber: secondNum)
+        result = calcMult.multiply(firstNum, secondNumber: secondNum)
+        displayResults()
+    }
+    
+  
+    @IBAction func onDividePressed(sender: UIButton) {
+        getData()
+        let calcDiv = SimpleCalculator(firstNumber: firstNum, secondNumber: secondNum)
+        result = calcDiv.divide(firstNum, secondNumber: secondNum)
+        displayResults()
+    }
+    
+    @IBAction func onResetPressed(sender: UIButton) {
+        clearData()
+    }
+    
 
 
 }
